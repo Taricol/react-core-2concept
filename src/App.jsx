14e4e153221a0621bff0463ba375c702dc856b2d
Taricol <mozumder9122023@gmail.com>
2025-12-26 16:47:43 +0600

    useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import Counter from './Counter'
 import Batsman from './Batsman'
 import Users from './Users'
 import Friends from './Friends'
+import Posts from './Posts'
+import Players from './Players'
 import './App.css'
 import { Suspense } from 'react'
 
-const fetchUsers=fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res=>res.json())
+// const fetchUsers=fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(res=>res.json())
 
-const fetchFriends=async()=>{
-  const res=await fetch('https://jsonplaceholder.typicode.com/users');
-  return res.json();
-}
+// const fetchFriends=async()=>{
+//   const res=await fetch('https://jsonplaceholder.typicode.com/users');
+//   return res.json();
+// }
 
+
+// const fetchPosts= async() =>{
+//   const res=await fetch('https://jsonplaceholder.typicode.com/posts');
+//   return res.json();
+// }
 
 function App() {
-    const friendsPromise=fetchFriends();
+    // const friendsPromise=fetchFriends();
+
+    // const postsPromise=fetchPosts();
+
     function handleClick(){
       alert('I am clicked')
     }
@@ -32,13 +42,18 @@ function App() {
     <>
       
       <h3>Vite + React</h3>
+      <Players></Players>
+      {/* <Suspense fallback={<h4>Posts are coming...</h4>}>
+          <Posts postsPromise={postsPromise}></Posts>
+      </Suspense> */}
+
       {/* <Suspense fallback={<h3>Loading...</h3>}>
         <Users fetchUsers={fetchUsers}></Users>
       </Suspense> */}
 
-      <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+      {/* <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
           <Friends friendsPromise={friendsPromise}></Friends>
-      </Suspense>
+      </Suspense> */}
       <Batsman></Batsman>
       <Counter></Counter>
 
--- src/Friends.jsx
+++ src/Friends.jsx
@@ -1,5 +1,5 @@
 import { use } from "react"
-
+import Friend from "./Friend";
 export default function Friends({friendsPromise}){
 
 
@@ -10,6 +10,9 @@ export default function Friends({friendsPromise}){
 
         <div className="card">
             <h3>Friends : {friends.length}</h3>
+            {
+                friends.map(friend=> <Friend key={friend.id} friend={friend}></Friend>)
+            }
         </div>
     )
 }
